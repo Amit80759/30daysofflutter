1c1e49baab6e2dd290dc6fc851497091e3617983
Amit80759 <amitprajapati80759@gmail.com>
2021-10-10 14:05:49 +0530

    Day 3 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatelessWidget {
       body: Center(
           child: Container(
               child: Text("Welcome to $days days of flutter by $name"))),
-       drawer: Drawer(),       
+      drawer: Drawer(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/Pages/home_page.dart';
+
+import 'Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +10,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
